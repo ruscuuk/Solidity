b2f4a0bfae8864c607d49c4ec968d99bacb0deaf
Remix Workspace <>
2022-09-30 16:36:20 +0300

    Smart Contract Sample

---
+++ NewWorkspace.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+// contract HelloWorld {
+//     string public myString = "helloWorld";
+// }
+
+
+// Data Types - values and references
+
+// contract ValueTypes {
+//     bool public b = true;
+//     uint public u = 123;
+
+//     int public i=-123 //burada int kullanmak negatif değer alabilmemi sağladı
+
+//     int public minInt = type(int).min;
+//     int public maxInt = type(int).max;
+//     adress public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+// }
+
+
+
+
+
